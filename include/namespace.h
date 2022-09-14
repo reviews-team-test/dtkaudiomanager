31831ea9c89fdb2be3af06f502c6e3e4a4be7d2f
guoyao <guoyao@uniontech.com>
2022-09-14 17:59:22 +0800

    feat: change demo to audiomanager
    change demo to audiomanager
    
    Log:

--- include/namespace.h
+++ include/namespace.h
@@ -2,9 +2,9 @@
 //
 // SPDX-License-Identifier: LGPL-3.0-or-later
 
-#define DDEMO_NAMESPACE Dtk::Demo
+#define DAUDIOMANAGER_NAMESPACE Dtk::AudioManager
 
-#define DDEMO_USE_NAMESPACE using namespace DDEMO_NAMESPACE;
+#define DAUDIOMANAGER_USE_NAMESPACE using namespace DAUDIOMANAGER_NAMESPACE;
 
-#define DDEMO_BEGIN_NAMESPACE namespace Dtk { namespace Demo {
-#define DDEMO_END_NAMESPACE }}
+#define DAUDIOMANAGER_BEGIN_NAMESPACE namespace Dtk { namespace AudioManager {
+#define DAUDIOMANAGER_END_NAMESPACE }}
